01c3655ad3f719bc4b068a04dd4ff4828f8ae431
wundervaflja <gorysko@gmail.com>
2020-10-12 07:39:21 +0200

    Fixed exporter and bitbucket bug

--- src/bitbucket/bitbucket_runner.py
+++ src/bitbucket/bitbucket_runner.py
@@ -1,6 +1,6 @@
 import json
 
-from pybitbucket.auth import OAuth1Authenticator
+from pybitbucket.auth import BasicAuthenticator
 from pybitbucket.bitbucket import Client
 from pybitbucket.commit import Commit
 from pybitbucket.pullrequest import PullRequest
@@ -19,13 +19,26 @@ from src.bitbucket.bitbucket_user import get_user_info
 from src.utils import get_logger, get_config
 
 logger = get_logger("BitBucketAPI")
-oauth_authenticator = OAuth1Authenticator(get_config("bitbucket.client_key"), get_config("bitbucket.client_secret"))
-bitbucket = Client(oauth_authenticator)
+bitbucket_client = BasicAuthenticator(get_config("bitbucket.user_name"), get_config("bitbucket.app_password"), get_config("bitbucket.user_email"))
+bitbucket_client = Client(bitbucket_client)
 BATCH_SIZE = 100
 ONE_HOUR_SEC = 3600
 
 
+def add_remote_relationship_methods(self, data):
+    for name, url in BitbucketBase.links_from(data):
+        if (name not in [i.name for i in BitbucketSpecialAction]):
+            setattr(self, name, partial(
+                self.client.remote_relationship,
+                template=url))
+
+from pybitbucket.bitbucket import BitbucketBase, BitbucketSpecialAction
+from functools import partial
+
+BitbucketBase.add_remote_relationship_methods = add_remote_relationship_methods
+
 def run(endpoint):
+
     repositories = get_config("bitbucket.repositories")
     host = get_config("azimu_api.host")
     port = get_config("azimu_api.port")
@@ -44,7 +57,7 @@ def process_user(username, url):
     table_name = "bitbucket_user"
     user = None
     try:
-        user = User.find_user_by_username(username=username, client=bitbucket)
+        user = User.find_user_by_username(username=username, client=bitbucket_client)
         logger.debug("BitBucket user - {}".format(user))
     except HTTPError:
         logger.debug("No BitBucket user - {}".format(username))
@@ -62,9 +75,9 @@ def process_user(username, url):
 @limits(calls=100, period=ONE_HOUR_SEC)
 def process_repository(repository, url):
     table_name = "bitbucket_repository"
+    user_name = get_config("bitbucket.user_name")
     user_name, repo_name = repository.split("/")
-    repository = Repository.find_repository_by_name_and_owner(repository_name=repo_name, owner=user_name,
-                                                              client=bitbucket)
+    repository = Repository.find_repository_by_name_and_owner(repository_name=repo_name, owner=user_name, client=bitbucket_client)
     logger.debug("Bitbucket repository - {}".format(repository))
     repository_info = vars(get_repository_info(repository.data))
     logger.debug("BitBucket repository owner - {}".format(repository_info['owner_uuid']))
@@ -83,7 +96,7 @@ def process_commits(repository, url):
     table_name = "bitbucket_commit"
     commits = []
     user_name, repo_name = repository.split("/")
-    for commit in Commit.find_commits_in_repository(user_name, repo_name, client=bitbucket):
+    for commit in Commit.find_commits_in_repository(user_name, repo_name, client=bitbucket_client):
         logger.debug("BitBucket commit - {}".format(commit))
         commit_stats = get_commit_stats(repository, commit.hash)
         commit_info = get_commit_payload(repository, commit.data, commit_stats)
@@ -113,7 +126,7 @@ def process_commits(repository, url):
 def get_commit_stats(repo, commit_hash):
     user_name, repository_name = repo.split("/")
     url = "https://api.bitbucket.org/2.0/repositories/{username}/{repo_slug}/diffstat/{spec}"
-    r = oauth_authenticator.session.get(
+    r = bitbucket_client.session.get(
         url.format(username=user_name, repo_slug=repository_name, spec=commit_hash))
     total_lines_added = 0
     total_lines_removed = 0
@@ -133,7 +146,7 @@ def get_commit_stats(repo, commit_hash):
 def process_refs(repository, url):
     table_name = "bitbucket_ref"
     user_name, repository_name = repository.split("/")
-    refs = Ref.find_refs_in_repository(user_name, repository_name, client=bitbucket)
+    refs = Ref.find_refs_in_repository(user_name, repository_name, client=bitbucket_client)
     for ref in refs:
         logger.debug("Bitbucket ref - {}".format(ref))
         ref_info = vars(get_ref_info(ref.data, repository))
@@ -152,7 +165,7 @@ def process_pull_requests(repository, url, state):
     user_name, repository_name = repository.split("/")
     pull_requests = PullRequest.find_pullrequests_for_repository_by_state(repository_name=repository_name,
                                                                           owner=user_name,
-                                                                          client=bitbucket, state=state)
+                                                                          client=bitbucket_client, state=state)
     for pr in pull_requests:
         logger.debug("BitBucket pull request - {}".format(pr.data))
         pr_info = vars(get_pull_request_info(repository, pr.data))
